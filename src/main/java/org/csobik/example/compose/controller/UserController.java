1986b053ed0ba4a73d85d272fca7c6467392f99d
Jiri Sobotik <jiri.sobotik@actum.cz>
2018-11-30 16:41:03 +0100

    wrong import

--- src/main/java/org/csobik/example/compose/controller/UserController.java
+++ src/main/java/org/csobik/example/compose/controller/UserController.java
@@ -3,7 +3,6 @@ package org.csobik.example.compose.controller;
 import lombok.AllArgsConstructor;
 import org.csobik.example.compose.entity.User;
 import org.csobik.example.compose.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
